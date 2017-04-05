7dba480cb7838d94edeec20db0b68765d7c682ee
L7\Shiyas.V <Shiyas@litmus7.com>
2017-04-05 11:31:10 +0530

    small fixes

--- scripts/js/navbar.js
+++ scripts/js/navbar.js
@@ -1,15 +1,15 @@
+// toggles navigation lists
 function toggleList() {
-    var navList = document.getElementById("navigation-menu");
-    var navListItems = document.getElementsByClassName("header-nav-item");
-    console.log(navList);
+    var navList = document.getElementById("navigation-menu");	//get navbar element
+    var navListItems = document.getElementsByClassName("header-nav-item");	//get each list items
+    // if navigation list is closed, add responsive class to it
     if (navList.className === "header-nav") {
-        navList.className += " responsive";
+        navList.className += " expanded";		//navbar element expanded added
         for(i=0; i < navListItems.length; i++){
-        	console.log(navListItems[i]);
-        	navListItems[i].className += " responsive";
+        	navListItems[i].className += " expanded";		//each item element add expanded to classlist
         }
     } else {
-        navList.className = "header-nav";
+        navList.className = "header-nav";		//if expanded, its closed by removing class
         for(i=0; i<navListItems.length; i++){
         	navListItems[i].className = "header-nav-item";
         }
